02b50144888f358d1efed5073810d6d196ad3e13
Pallab Mandal <pallab@creativeideaz.io>
2025-09-20 17:43:45 +0000

    add should not queue

--- src/Exceptions/Publish/BaseException.php
+++ src/Exceptions/Publish/BaseException.php
@@ -2,9 +2,10 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Contracts\Debug\ShouldntReport;
 use Xetreon\JsonResponse\Exceptions\BaseException as CoreException;
 
-class BaseException extends CoreException
+class BaseException extends CoreException implements ShouldntReport
 {
 
 }
